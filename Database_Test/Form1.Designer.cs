fbf5c4c91271b443840729452f6e1c27b3e40068
Unity2033 <dlfjswodlf@naver.com>
2024-02-29 17:52:07 +0900

    (2024.02.29) winform database 만들기

--- Database_Test/Form1.Designer.cs
+++ Database_Test/Form1.Designer.cs
@@ -29,26 +29,29 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.RegionSelectBox = new System.Windows.Forms.ComboBox();
+            this.RaceText = new System.Windows.Forms.TextBox();
+            this.NameText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.buttonClear = new System.Windows.Forms.Button();
-            this.buttonCreate = new System.Windows.Forms.Button();
+            this.IDText = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox = new System.Windows.Forms.ComboBox();
+            this.dataGridViewInfo = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -61,31 +64,19 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "information";
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.buttonRemove);
-            this.groupBox2.Controls.Add(this.buttonCreate);
-            this.groupBox2.Controls.Add(this.buttonClear);
-            this.groupBox2.Location = new System.Drawing.Point(417, 35);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(305, 167);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "control";
-            // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
-            this.tableLayoutPanel1.Controls.Add(this.textBox4, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.RegionSelectBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.RaceText, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.NameText, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.IDText, 1, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 20);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
@@ -95,6 +86,32 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(258, 133);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // RegionSelectBox
+            // 
+            this.RegionSelectBox.FormattingEnabled = true;
+            this.RegionSelectBox.Items.AddRange(new object[] {
+            "1지역",
+            "2지역",
+            "3지역"});
+            this.RegionSelectBox.Location = new System.Drawing.Point(89, 102);
+            this.RegionSelectBox.Name = "RegionSelectBox";
+            this.RegionSelectBox.Size = new System.Drawing.Size(166, 20);
+            this.RegionSelectBox.TabIndex = 4;
+            // 
+            // RaceText
+            // 
+            this.RaceText.Location = new System.Drawing.Point(89, 69);
+            this.RaceText.Name = "RaceText";
+            this.RaceText.Size = new System.Drawing.Size(166, 21);
+            this.RaceText.TabIndex = 6;
+            // 
+            // NameText
+            // 
+            this.NameText.Location = new System.Drawing.Point(89, 36);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(166, 21);
+            this.NameText.TabIndex = 5;
             // 
             // label1
             // 
@@ -132,65 +149,62 @@
             this.label4.Text = "Region";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // IDText
             // 
-            this.textBox1.Location = new System.Drawing.Point(89, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(166, 21);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.IDText.Location = new System.Drawing.Point(89, 3);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(166, 21);
+            this.IDText.TabIndex = 4;
+            this.IDText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // groupBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(89, 36);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(166, 21);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(89, 69);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(166, 21);
-            this.textBox3.TabIndex = 6;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(89, 102);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(166, 21);
-            this.textBox4.TabIndex = 7;
-            // 
-            // buttonClear
-            // 
-            this.buttonClear.Location = new System.Drawing.Point(25, 23);
-            this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(264, 23);
-            this.buttonClear.TabIndex = 0;
-            this.buttonClear.Text = "Clear";
-            this.buttonClear.UseVisualStyleBackColor = true;
-            // 
-            // buttonCreate
-            // 
-            this.buttonCreate.Location = new System.Drawing.Point(25, 70);
-            this.buttonCreate.Name = "buttonCreate";
-            this.buttonCreate.Size = new System.Drawing.Size(264, 23);
-            this.buttonCreate.TabIndex = 1;
-            this.buttonCreate.Text = "Create";
-            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.buttonLoad);
+            this.groupBox2.Controls.Add(this.buttonSave);
+            this.groupBox2.Controls.Add(this.buttonRemove);
+            this.groupBox2.Controls.Add(this.buttonCreate);
+            this.groupBox2.Controls.Add(this.buttonClear);
+            this.groupBox2.Location = new System.Drawing.Point(417, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 167);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "control";
             // 
             // buttonRemove
             // 
             this.buttonRemove.Location = new System.Drawing.Point(25, 119);
             this.buttonRemove.Name = "buttonRemove";
-            this.buttonRemove.Size = new System.Drawing.Size(264, 23);
+            this.buttonRemove.Size = new System.Drawing.Size(126, 23);
             this.buttonRemove.TabIndex = 2;
             this.buttonRemove.Text = "Remove";
             this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(25, 70);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(126, 23);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(25, 23);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(126, 23);
+            this.buttonClear.TabIndex = 0;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Controls.Add(this.listBox);
+            this.groupBox3.Controls.Add(this.dataGridViewInfo);
             this.groupBox3.Location = new System.Drawing.Point(48, 222);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(674, 216);
@@ -198,31 +212,64 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "List";
             // 
-            // dataGridView1
+            // listBox
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 37);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(642, 150);
-            this.dataGridView1.TabIndex = 4;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Items.AddRange(new object[] {
+            "1지역",
+            "2지역",
+            "3지역"});
+            this.listBox.Location = new System.Drawing.Point(35, 11);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(143, 20);
+            this.listBox.TabIndex = 4;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // dataGridViewInfo
+            // 
+            this.dataGridViewInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInfo.Location = new System.Drawing.Point(16, 37);
+            this.dataGridViewInfo.Name = "dataGridViewInfo";
+            this.dataGridViewInfo.RowTemplate.Height = 23;
+            this.dataGridViewInfo.Size = new System.Drawing.Size(642, 150);
+            this.dataGridViewInfo.TabIndex = 4;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(163, 23);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(126, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "dataSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(163, 70);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(126, 23);
+            this.buttonLoad.TabIndex = 4;
+            this.buttonLoad.Text = "dataLoad";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1071, 450);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -235,16 +282,19 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox IDText;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox RaceText;
+        private System.Windows.Forms.TextBox NameText;
         private System.Windows.Forms.Button buttonRemove;
         private System.Windows.Forms.Button buttonCreate;
         private System.Windows.Forms.Button buttonClear;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewInfo;
+        private System.Windows.Forms.ComboBox RegionSelectBox;
+        private System.Windows.Forms.ComboBox listBox;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
     }
 }
 
--- Database_Test/Form1.cs
+++ Database_Test/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,8 @@ namespace Database_Test
 {
     public partial class Form1 : Form
     {
-        const float number = 10;
-
-        // 데이터들을 자료구조 collection 묵어서 사용을 해볼겁니다.
-        // 클래스 내부에서 직접 값을 설정하고 데이터 처리하는 대부분 방식은 하드코딩으로 처리됩니다.
+        // Row - Table - Set 데이터가 저장되는 크기에 따라서 분류 잠시 소개를 드렸습니다.
+        DataSet ds = new DataSet();
 
         public Form1()
         {
@@ -25,6 +24,158 @@ namespace Database_Test
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            IDText.Text = "";
+            NameText.Text = "";
+            RaceText.Text = "";
+            RegionSelectBox.Text = "";
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            bool checkIsTable = false;
+
+            if (ds.Tables.Contains(RegionSelectBox.Text)) // 키값이 이미 존재합니다.
+            {
+                checkIsTable = true;
+            }
+
+            // bool 이 True이면 이미 생성한 데이터가 있으니까 그 데이터를 불러와서 추가로 데이터를 만들어 주면 되고
+            // bool 이 false이면 처음 생성한 데이터가 이기 때문에 새롭게 데이터를 생성해주면 됩니다.
+
+            // DataSet - DataTable - DataRow, DataColumn 생성
+            DataTable dt = null;
+
+            if (!checkIsTable)
+            {
+                // 데이터가 없을 경우에는 새롭게 데이터를 만들어 준다.
+                dt = new DataTable(listBox.Text);
+
+                DataColumn columnID = new DataColumn("ID", typeof(string));
+                DataColumn columnName = new DataColumn("Name", typeof(string));
+                DataColumn columnRace = new DataColumn("Race", typeof(string));
+                DataColumn columnRegion = new DataColumn("Region", typeof(string));
+
+                dt.Columns.Add(columnID);
+                dt.Columns.Add(columnName);
+                dt.Columns.Add(columnRace);
+                dt.Columns.Add(columnRegion);
+            }
+            else
+            {
+                dt = ds.Tables[RegionSelectBox.Text];
+            }
+
+            // 생성할 데이터를 Row에 입력을 한 후
+            DataRow row = dt.NewRow();
+            row["ID"] = IDText.Text;
+            row["Name"] = NameText.Text;
+            row["Race"] = RaceText.Text;
+            row["Region"] = RegionSelectBox.Text;
+
+            // 데이터 테이블이 이미 존재한다면 기존 테이블을 불러온 후 새로운 데이터를 이어서 추가 해주고
+            if (checkIsTable)
+            {
+                ds.Tables[RegionSelectBox.Text].Rows.Add(row);
+            }
+            else
+            {
+                // 테이블이 존재 하지 않는다면 row를 table에 추가해주면 된다.
+                dt.Rows.Add(row);
+                ds.Tables.Add(dt);
+
+            }
+
+            // 생성한 데이터를 볼 수 있게 View
+            ViewRefresh();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            int selectRow = dataGridViewInfo.SelectedRows[0].Index;
+
+            ds.Tables[RegionSelectBox.Text].Rows.RemoveAt(selectRow);
+
+            ViewRefresh();
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridViewInfo.DataSource = ds.Tables[listBox.Text];
+
+            foreach(DataGridViewRow row in dataGridViewInfo.Rows)
+            {
+                row.HeaderCell.Value = row.Index.ToString();
+            }
+        }
+
+        private void ViewRefresh()
+        {
+            dataGridViewInfo.DataSource = ds.Tables[listBox.Text];
+
+            foreach (DataGridViewRow row in dataGridViewInfo.Rows)
+            {
+                row.HeaderCell.Value = row.Index.ToString();
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            TextWriter writer = new StreamWriter(@"C:\Sample\Text.txt");
+
+            for (int i = 0; i < dataGridViewInfo.Rows.Count - 1; i++)
+            {
+                for(int j = 0; j< dataGridViewInfo.Columns.Count; j++)
+                {
+                    writer.Write("\t" + dataGridViewInfo.Rows[i].Cells[j].Value.ToString() + "\t" + "|");
+                }
+                writer.WriteLine();
+            }
+
+            writer.Close();
+            MessageBox.Show("데이터가 출력되었음");
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(@"C:\Sample\Text.txt");
+
+            string[] lines = File.ReadAllLines(@"C:\Sample\Text.txt");
+
+            // Seperator 어떤 것으로 하냐에 따라서 Format형식이 바뀐다.
+            dataGridViewInfo.ColumnCount = lines[0].Split('|').Length;
+
+            DataTable dt = new DataTable("1지역");
+            dt = new DataTable(listBox.Text);
+            ds.Tables.Add(dt);
+
+            DataColumn columnID = new DataColumn("ID", typeof(string));
+            DataColumn columnName = new DataColumn("Name", typeof(string));
+            DataColumn columnRace = new DataColumn("Race", typeof(string));
+            DataColumn columnRegion = new DataColumn("Region", typeof(string));
+
+            dt.Columns.Add(columnID);
+            dt.Columns.Add(columnName);
+            dt.Columns.Add(columnRace);
+            dt.Columns.Add(columnRegion);
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split('|');
+
+                DataRow row = dt.NewRow();
+                row["ID"] = values[0];
+                row["Name"] = values[1];
+                row["Race"] = values[2];
+                row["Region"] = values[3];
+
+                ds.Tables[listBox.Text].Rows.Add(row);
+            }
+
+            ViewRefresh();
         }
     }
 }
